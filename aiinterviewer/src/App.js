d938226e691d036b42dbd2cad08effe3feb3002c
Pranjal Aggarwal <54495980+pranjalagg@users.noreply.github.com>
2024-02-23 02:59:33 -0700

    some progress

--- aiinterviewer/src/App.js
+++ aiinterviewer/src/App.js
@@ -1,20 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-import RecordView from './components/record';
-import React, {useState, useEffect} from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import RecordView from "./components/record";
+import React, { useState, useEffect } from "react";
+// import Transcribe from './components/transcribe';
+import OpenAI from "openai";
 
 function App() {
+  const client = new OpenAI({
+    apiKey: `${process.env.REACT_APP_OPENAI_API_KEY}`,
+    dangerouslyAllowBrowser: true,
+  });
 
-  const [media, setMedia] = useState(null);
+  const [audioFile, setMedia] = useState(null);
+  const [transcript, setTranscript] = useState("");
+
+  const handleTranscribe = async () => {
+    console.log("Here");
+    const setTranscript = await client.audio.transcriptions.create({
+      model: "whisper-1",
+      audio: "aiinterviewer/src/sample.mp3",
+    });
+    console.log("Transcribing", transcript);
+  };
 
   useEffect(() => {
-    console.log("main", media);
-  }, [media]);
+    if (audioFile !== null) {
+      console.log("main", audioFile);
+      // Transcribe(audioFile);
+    }
+  }, [audioFile]);
 
   return (
     <div className="App">
       <header className="App-header">
         <RecordView setMedia={setMedia} />
+        <button onClick={handleTranscribe}>Transcribe</button>
       </header>
     </div>
   );
