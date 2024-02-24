4793772a73efb840c971a734454100cb85d2356e
Pranjal Aggarwal <54495980+pranjalagg@users.noreply.github.com>
2024-02-24 01:37:01 -0700

    audio blob

--- aiinterviewer/src/App.js
+++ aiinterviewer/src/App.js
@@ -4,6 +4,7 @@ import RecordView from "./components/record";
 import React, { useState, useEffect } from "react";
 // import Transcribe from './components/transcribe';
 import OpenAI from "openai";
+import { toFile } from 'openai/uploads';
 
 function App() {
   const client = new OpenAI({
@@ -12,15 +13,47 @@ function App() {
   });
 
   const [audioFile, setMedia] = useState(null);
+  const [audioData, setAudioData] = useState(null);
   const [transcript, setTranscript] = useState("");
 
-  const handleTranscribe = async () => {
-    console.log("Here");
-    const setTranscript = await client.audio.transcriptions.create({
+
+
+useEffect(()=> {
+  // console.log("audioData", audioData);
+  if (audioData !== null) {
+    // console.log("Here");
+    setTranscript(client.audio.transcriptions.create({
       model: "whisper-1",
-      audio: "aiinterviewer/src/sample.mp3",
-    });
-    console.log("Transcribing", transcript);
+      file: toFile(audioData),
+    }));
+    if (transcript !== null) {
+      console.log(transcript);
+    }
+    else{
+      console.log("Transcript is null");
+    }
+    }
+}, [audioData]);
+
+
+const handleTranscribe = () => {
+  fetch(audioFile)
+  .then(response => response.blob())
+  .then(blob => {
+    console.log("blob", blob);
+    // Create a FileReader
+    const reader = new FileReader();
+    // Read the blob as Data URL
+    reader.readAsDataURL(blob)
+    reader.onload = function() {
+      setAudioData(blob);
+
+      // console.log('Audio data:', audioData);
+    };
+  })
+  .catch(error => {
+    console.error('Error fetching the audio file:', error);
+  });
   };
 
   useEffect(() => {
